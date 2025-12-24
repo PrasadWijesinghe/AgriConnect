25d2c0a39206edd1a3eb804922a1c4e7c70a36ad
PrasadWijesinghe <wmprasadwijesinghe@gmail.com>
2025-12-24 15:19:03 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
 import CropRecommendations from './pages/CropRecommendations';
 import AIChatbot from './pages/AIChatbot';
